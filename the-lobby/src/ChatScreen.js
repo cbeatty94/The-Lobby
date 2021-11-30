556ecbc31f16fbf81a387a50115b1ba66bf385d9
Burke <beatty.burke@gmail.com>
2021-11-30 11:47:36 -0800

    styled the text on chat page, specifically user vs matches

--- the-lobby/src/ChatScreen.js
+++ the-lobby/src/ChatScreen.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import Avatar from '@mui/material/Avatar';
+import "./ChatScreen.css"
 
 function ChatScreen() {
     const [messages, setMessages] = useState([
@@ -8,15 +10,36 @@ function ChatScreen() {
             message: "Want to group up?"
         },
         {
-            name: "Dolphin",
-            image: "https://cdn.pixabay.com/photo/2016/07/30/21/37/mario-1558062_960_720.jpg",
+            name: "BadLarry",
+            image: "https://cdn.pixabay.com/photo/2016/07/30/14/28/yoschi-1557253_960_720.jpg",
             message: "What role do you main in Overwatch?"
+        },
+        {
+            message: "I am a tank main, but I also like playing Ana!"
         }
     ])
 
     return (
         <div className="chatScreen">
-            <p>YOU MATCHED WITH BADLARRY ON 11/24/21</p>
+            <p className="chatScreen__timestamp">YOU MATCHED WITH BADLARRY ON 11/24/21</p>
+            {messages.map(message => 
+                message.name ? (
+                    <div className="chatScreen__message">
+                        <Avatar
+                            className="chatScreen__image"
+                            alt={message.name}
+                            src={message.image}
+                        />
+                        <p className="chatScreen__text">{message.message}</p>
+                    </div>
+                ) : (
+                    <div className="chatScreen__message">
+                        <p className="chatScreen__textUser">{message.message}</p>
+                    </div>
+                )
+                
+                )}
+
         </div>
     )
 }
