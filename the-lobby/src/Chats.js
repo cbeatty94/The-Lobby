3b826f054bd8cf37a9e5ae6c819bc679fc12ceea
Burke <beatty.burke@gmail.com>
2021-11-30 11:11:23 -0800

    setting up chat page and style

--- the-lobby/src/Chats.js
+++ the-lobby/src/Chats.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Chats.css";
+import Chat from "./Chat.js";
 
 function Chats() {
     return (
         <div className="chats">
+            <Chat
+                name="BadLarry"
+                message="Want to group up?"
+                timestamp="17 minutes ago"
+                profilePic="https://cdn.pixabay.com/photo/2016/07/30/14/28/yoschi-1557253_960_720.jpg"
+            />
+             <Chat
+                name="Dolphin"
+                message="What role do you main in Overwatch?"
+                timestamp="32 minutes ago"
+                profilePic="https://cdn.pixabay.com/photo/2016/07/30/21/37/mario-1558062_960_720.jpg"
+            />
+
             
         </div>
     )
